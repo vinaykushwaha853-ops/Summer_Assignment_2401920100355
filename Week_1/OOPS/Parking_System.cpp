d05d6f8b6d4aa1447ac5f82eeef284f25cd8cddf
vinaykushwaha853-ops <vinaykushwaha853@gmail.com>
2026-06-06 18:21:56 +0530

    Implement ParkingSystem class with addCar method

---
+++ Week_1/OOPS/Parking_System.cpp
@@ -0,0 +1,35 @@
+class ParkingSystem {
+public:
+    int big, medium, small;
+
+    ParkingSystem(int big, int medium, int small) {
+        this->big = big;
+
+        this->medium = medium;
+
+        this->small = small;
+    }
+
+    bool addCar(int carType) {
+        if (carType == 1) {
+            if (big > 0) {
+                big--;
+                return true;
+            }
+        }
+        else if (carType == 2)  {
+            if (medium > 0) {
+                medium--;
+                return true;
+            }
+        }
+        else {
+            if (small > 0) {
+                small--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
